446e24d94fd18db808ac7487e73ca80182b298db
gunnar.behrens <gunnar.behrens@vwfs.io>
2023-07-09 20:53:58 +0200

    check scikitlearn version

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -1,8 +1,19 @@
 import pickle
 from main import clf
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+
+from sklearn.exceptions import InconsistentVersionWarning
+warnings.simplefilter("error", InconsistentVersionWarning)
 
 
 def test_accuracy():
+
+    try:
+        est = pickle.loads("data/test_data.pkl")
+    except InconsistentVersionWarning as w:
+        print(w.original_sklearn_version)
+
 
     # Load test data
     with open("data/test_data.pkl", "rb") as file:
@@ -16,3 +27,15 @@ def test_accuracy():
 
     # Accuracy should be over 90%
     assert acc > 0.9
+
+
+def test_pipeline_and_scaler():
+    # Check if clf is an instance of sklearn.pipeline.Pipeline
+    isPipeline = isinstance(clf, Pipeline)
+    assert isPipeline
+
+    if isPipeline:
+        # Check if first step of pipeline is an instance of
+        # sklearn.preprocessing.StandardScaler
+        firstStep = [v for v in clf.named_steps.values()][0]
+        assert isinstance(firstStep, StandardScaler)
